71f565b88ddd3d26c3cbc07f285703d678d765b8
ofkarakus <omerfarukkarakus01@gmail.com>
2020-10-18 22:20:42 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
